ea31a33864bff44f321415bc626c601881ce4443
wangmeng <wangmengc@uniontech.com>
2025-08-14 15:01:17 +0800

    init

---
+++ common/src/lib.rs
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
