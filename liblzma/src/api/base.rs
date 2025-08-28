ec60f4f454112a0ba5073df97d974ec481bb7f33
wangmengc <wangmengc@uniontech.com>
2025-08-28 09:59:26 +0800

    liblzma/src/common/common.rs中添加部分结构体

--- liblzma/src/api/base.rs
+++ liblzma/src/api/base.rs
@@ -6,3 +6,21 @@
 
 pub type LzmaBool = u8;
 pub type LzmaReservedEnum = u32;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LzmaRet {
+    Ok = 0,
+    StreamEnd = 1,
+    NoCheck = 2,
+    UnsupportedCheck = 3,
+    GetCheck = 4,
+    MemError = 5,
+    MemlimitError = 6,
+    FormatError = 7,
+    OptionsError = 8,
+    DataError = 9,
+    BufError = 10,
+    ProgError = 11,
+    SeekNeeded = 12,
+    RetInternal1 = 13,
+}
--- liblzma/src/common/mod.rs
+++ liblzma/src/common/mod.rs
@@ -3,6 +3,10 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-
+#![allow(clippy::module_inception)]
+#![allow(clippy::type_complexity)]
+pub mod common;
 pub mod index_tree;
+
+pub use common::*;
 pub use index_tree::*;
