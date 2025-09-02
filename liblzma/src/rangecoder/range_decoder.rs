fdbe68ec226960372425232ab2cc1e0ad91a6178
wangmengc <wangmengc@uniontech.com>
2025-09-02 09:34:20 +0800

    完善liblzma/src/rangecoder/range_decoder.rs文件

--- liblzma/src/rangecoder/range_decoder.rs
+++ liblzma/src/rangecoder/range_decoder.rs
@@ -104,3 +104,104 @@ macro_rules! rc_if_0 {
         $rc.code < $rc_bound
     }};
 }
+
+#[macro_export]
+macro_rules! rc_update_0 {
+    ($rc:expr, $prob:expr, $rc_bound:expr) => {
+        $rc.range = $rc_bound;
+        $prob = (($prob as u32).wrapping_add(
+            ((crate::rangecoder::range_common::RC_BIT_MODEL_TOTAL - $prob as u32)
+                >> crate::rangecoder::range_common::RC_MOVE_BITS),
+        )) as u16;
+    };
+}
+
+#[macro_export]
+macro_rules! rc_update_1 {
+    ($rc:expr, $prob:expr, $rc_bound:expr) => {
+        $rc.range = $rc.range.wrapping_sub($rc_bound);
+        $rc.code = $rc.code.wrapping_sub($rc_bound);
+        $prob =
+            ($prob as u32 - ($prob as u32 >> crate::rangecoder::range_common::RC_MOVE_BITS)) as u16;
+    };
+}
+
+#[macro_export]
+macro_rules! rc_bit_last {
+    ($rc:expr, $prob:expr, $action0:expr, $action1:expr, $seq:expr, $in_pos:expr, $in_size:expr, $input:expr) => {
+        if rc_if_0!($rc, $prob, $seq, $in_pos, $in_size, $input) {
+            rc_update_0!($rc, $prob, rc_bound);
+            $action0;
+        } else {
+            rc_update_1!($rc, $prob, rc_bound);
+            $action1;
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! rc_bit {
+    ($rc:expr, $prob:expr, $symbol:expr, $action0:expr, $action1:expr, $in_pos:expr, $in_size:expr, $input:expr, $rc_bound:expr,$seq:expr) => {{
+        if $rc.range < crate::rangecoder::range_common::RC_TOP_VALUE {
+            if $in_pos == $in_size {
+                $seq;
+                break;
+            }
+            $rc.range <<= crate::rangecoder::range_common::RC_SHIFT_BITS;
+            $rc.code = ($rc.code << crate::rangecoder::range_common::RC_SHIFT_BITS)
+                | ($input[$in_pos] as u32);
+            $in_pos += 1;
+        }
+        $rc_bound = ($rc.range >> crate::rangecoder::range_common::RC_BIT_MODEL_TOTAL_BITS)
+            .wrapping_mul($prob as u32);
+        if $rc.code < $rc_bound {
+            rc_update_0!($rc, $prob, $rc_bound);
+            $symbol = ($symbol << 1);
+            $action0;
+        } else {
+            rc_update_1!($rc, $prob, $rc_bound);
+            $symbol = ($symbol << 1) + 1;
+            $action1;
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! rc_bit_encode {
+    ($rc:expr, $prob:expr, $bit:expr) => {
+        $rc.symbols[$rc.count] = RcSymbol::try_from($bit).unwrap();
+        $rc.probs[$rc.count] = *$prob;
+        $rc.count += 1;
+    };
+}
+
+#[macro_export]
+macro_rules! rc_direct {
+    ($rc:expr, $dest:expr, $seq:expr, $in_pos:expr, $in_size:expr, $input:expr, $rc_bound:expr) => {
+        if $rc.range < crate::rangecoder::range_common::RC_TOP_VALUE {
+            if $in_pos == $in_size {
+                $seq;
+                break;
+            }
+            $rc.range <<= crate::rangecoder::range_common::RC_SHIFT_BITS;
+            $rc.code = ($rc.code << crate::rangecoder::range_common::RC_SHIFT_BITS)
+                | ($input[$in_pos] as u32);
+            $in_pos += 1;
+        }
+
+        $rc.range >>= 1;
+        $rc.code = $rc.code.wrapping_sub($rc.range);
+        $rc_bound = 0u32.wrapping_sub($rc.code >> 31);
+        $rc.code = $rc.code.wrapping_add($rc.range & $rc_bound);
+        $dest = ($dest << 1);
+        $dest = $dest.wrapping_add($rc_bound.wrapping_add(1));
+    };
+}
+
+#[macro_export]
+macro_rules! rc_bit_case {
+    ($prob:expr, $action0:expr, $action1:expr, $seq:expr) => {
+        case $seq:
+            rc_bit!($prob, $action0, $action1, $seq);
+    };
+}
