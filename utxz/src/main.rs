455a053f17cb376d18ef4bf5d39a33e401749e38
liangyangang <liangyangang@uniontech.com>
2025-12-08 11:14:40 +0800

    feat: 新增utxz/src/util.rs，实现常用工具函数

--- utxz/src/main.rs
+++ utxz/src/main.rs
@@ -4,6 +4,31 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+mod args;
+mod coder;
+mod file_io;
+mod hardware;
+mod list;
+mod message;
+mod mytime;
+mod options;
+mod signals;
+mod suffix;
+mod util;
+
+const E_SUCCESS: i32 = 0;
+const E_ERROR: i32 = 1;
+const E_WARNING: i32 = 2;
+
+// 定义 exit_status_type 枚举
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)] // 确保与 C 的内存布局兼容
+pub enum ExitStatusType {
+    ESuccess = 0,
+    EError = 1,
+    EWarning = 2,
+}
+
 fn main() {
     println!("Hello, world!");
 }
