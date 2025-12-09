32a93c1f75beb0e512e2d540ebdcb9b2b01304e5
xingwei-liu <liuxingwei@uniontech.com>
2025-12-09 17:01:40 +0800

    feat: 增加多个模块，并开放模块中的接口

--- liblzma/src/common/mod.rs
+++ liblzma/src/common/mod.rs
@@ -3,6 +3,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
+
 #![allow(clippy::module_inception)]
 #![allow(clippy::type_complexity)]
 #![allow(clippy::new_without_default)]
@@ -11,86 +12,86 @@
 #![allow(clippy::clone_on_copy)]
 #![allow(clippy::enum_variant_names)]
 #![allow(private_interfaces)]
+
+use std::{cell::RefCell, ptr};
+
+use crate::api::{LzmaAction, LzmaCheck, LzmaFilter, LzmaRet, LzmaStream, LzmaVli};
+
+pub mod common;
+pub use common::*;
+
 pub mod alone_decoder;
 pub mod alone_encoder;
-pub mod common;
-pub mod index_tree;
-pub use alone_decoder::*;
-pub use alone_encoder::*;
-
 pub mod auto_decoder;
-pub use auto_decoder::*;
-
-pub use common::*;
-pub use index_tree::*;
-
 pub mod block_decoder;
 pub mod block_encoder;
-pub use block_decoder::*;
-pub use block_encoder::*;
-
+pub mod block_header_decoder;
+pub mod block_header_encoder;
+pub mod block_util;
 pub mod file_info;
+pub mod filter_buffer_decoder;
+pub mod filter_buffer_encoder;
 pub mod filter_common;
 pub mod filter_decoder;
 pub mod filter_encoder;
+pub mod filter_flags_decoder;
+pub mod filter_flags_encoder;
 pub mod index;
 pub mod index_decoder;
 pub mod index_encoder;
 pub mod index_hash;
+pub mod lzip_decoder;
+pub mod memcmplen;
+pub mod microlzma_decoder;
+pub mod microlzma_encoder;
+pub mod stream_decoder;
+pub mod stream_encoder;
+pub mod stream_flags_commom;
+pub mod stream_flags_decoder;
+pub mod stream_flags_encoder;
+pub mod vli_decoder;
+pub mod vli_encoder;
+pub mod vli_size;
+
+pub mod index_tree;
+pub use index_tree::*;
+
 pub mod hardware_cputhreads;
 pub use hardware_cputhreads::*;
 
 pub mod hardware_physmem;
 pub use hardware_physmem::*;
 
+pub use alone_decoder::*;
+pub use alone_encoder::*;
+pub use auto_decoder::*;
+pub use block_decoder::*;
+pub use block_encoder::*;
+pub use block_header_decoder::*;
+pub use block_header_encoder::*;
+pub use block_util::*;
 pub use file_info::*;
+pub use filter_buffer_decoder::*;
+pub use filter_buffer_encoder::*;
 pub use filter_common::*;
 pub use filter_decoder::*;
 pub use filter_encoder::*;
+pub use filter_flags_decoder::*;
+pub use filter_flags_encoder::*;
 pub use index::*;
 pub use index_decoder::*;
 pub use index_encoder::*;
 pub use index_hash::*;
-
-pub mod vli_size;
-pub use vli_size::*;
-
-pub mod lzip_decoder;
-pub mod microlzma_decoder;
-pub mod microlzma_encoder;
-pub mod stream_decoder;
-pub mod stream_encoder;
-
 pub use lzip_decoder::*;
+pub use memcmplen::*;
 pub use microlzma_decoder::*;
 pub use microlzma_encoder::*;
 pub use stream_decoder::*;
 pub use stream_encoder::*;
-
-pub mod stream_flags_commom;
-pub mod stream_flags_decoder;
-pub mod stream_flags_encoder;
 pub use stream_flags_commom::*;
 pub use stream_flags_decoder::*;
 pub use stream_flags_encoder::*;
-
-pub mod vli_decoder;
-pub mod vli_encoder;
 pub use vli_decoder::*;
 pub use vli_encoder::*;
-
-pub mod memcmplen;
-pub use memcmplen::*;
-
-pub mod block_util;
-pub use block_util::*;
-
-pub mod block_header_decoder;
-pub mod block_header_encoder;
-pub use block_header_decoder::*;
-pub use block_header_encoder::*;
-
-pub mod filter_flags_decoder;
-pub mod filter_flags_encoder;
-pub use filter_flags_decoder::*;
-pub use filter_flags_encoder::*;
+pub use vli_size::*;
+pub mod string_conversion;
--- utxz/src/args.rs
+++ utxz/src/args.rs
@@ -9,7 +9,7 @@ use std::error::Error;
 use std::str;
 use std::sync::Mutex;
 
- lazy_static! {
+lazy_static! {
     pub static ref OPT_STDOUT: Mutex<bool> = Mutex::new(false);
     pub static ref OPT_FORCE: Mutex<bool> = Mutex::new(false);
     pub static ref OPT_KEEP_ORIGINAL: Mutex<bool> = Mutex::new(false);
